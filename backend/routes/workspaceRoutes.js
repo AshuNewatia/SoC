117d6e0f1949eb202b0624ecb47caf1ccc7b2ca1
Harshk013 <harshkasera262@gmail.com>
2026-06-20 14:32:11 +0530

    issue fix

--- backend/routes/workspaceRoutes.js
+++ backend/routes/workspaceRoutes.js
@@ -1,40 +1,19 @@
 import express from "express";
-
 import {
   createWorkspace,
   getWorkspaces,
   getWorkspaceById,
+  updateWorkspace,
   deleteWorkspace,
-  updateWorkspace
 } from "../controllers/workspaceController.js";
-
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post(
-  "/workspaces",protect,
-  createWorkspace
-);
-
-router.get(
-  "/workspaces",protect,
-  getWorkspaces
-);
-
-router.get(
-  "/workspaces/:workspaceId",protect,
-  getWorkspaceById
-);
-
-router.put(
-  "/workspaces/:workspaceId",protect,
-  updateWorkspace
-);
-
-router.delete(
-  "/workspaces/:workspaceId",protect,
-  deleteWorkspace
-);
+router.post("/workspaces", protect, createWorkspace);
+router.get("/workspaces", protect, getWorkspaces);
+router.get("/workspaces/:workspaceId", protect, getWorkspaceById);
+router.put("/workspaces/:workspaceId", protect, updateWorkspace);
+router.delete("/workspaces/:workspaceId", protect, deleteWorkspace);
 
 export default router;
