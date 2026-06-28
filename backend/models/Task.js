754b3373f37fdcff7c02ed66cdd4aff20fbf83b9
mahisingh06330-arch <mahisingh06330@gmail.com>
2026-06-28 12:33:57 +0530

    Analytics

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -5,7 +5,7 @@ const taskSchema = new mongoose.Schema(
         title: { type: String, required: true },
         description: { type: String },
         priority: { type: String, enum: ["Low","Medium","High","Critical"], required: true },
-        status: { type: String, enum: ["todo","progress","completed"] , deafult: "todo"},
+        status: { type: String, enum: ["todo","progress","completed"] , default: "todo"},
         workspace: { type: mongoose.Schema.Types.ObjectId, ref: "Workspace", required: true },
         assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}],
         createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
