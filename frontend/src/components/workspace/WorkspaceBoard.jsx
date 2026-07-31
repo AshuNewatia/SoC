d1a34803e9d22ad4b9ecf24ed2d0ae1aa7a05550
Ashu <govind06092007@gmail.com>
2026-07-31 19:43:52 +0530

    Github Change 7

--- frontend/src/components/workspace/WorkspaceBoard.jsx
+++ frontend/src/components/workspace/WorkspaceBoard.jsx
@@ -1,5 +1,5 @@
 import api from "../../services/api";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { Plus } from "lucide-react";
 import { Search, X } from "lucide-react";
