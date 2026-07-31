0765b455d8dc0713c1e259b668b79ac9c207d55b
Ashu <govind06092007@gmail.com>
2026-07-31 20:10:25 +0530

    Github Change 8

--- backend/routes/webhookRoutes.js
+++ backend/routes/webhookRoutes.js
@@ -3,6 +3,10 @@ import { handleGithubWebhook } from "../controllers/webhookController.js";
 
 const router = express.Router();
 
+router.get("/github", (req, res) => {
+  res.send("Webhook route is active! Send a POST request from GitHub.");
+});
+
 router.post("/github", handleGithubWebhook);
 
 export default router;
